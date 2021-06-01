30f34e1b0fec53e84db5a4ae2dfb27c63e5abebc
aguerosys <aguerolinkk@gmail.com>
2021-06-01 16:17:48 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    </head>
+    <body class="bg-gray-200 py-10">
+        
+        <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+
+            @if ($errors->any())
+                <ul class="list-none p-4 mb-4 bg-red-100 text-red-500">
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            @endif
+
+
+
+            <form action="tags" method="POST" class="flex mb-4">
+
+                @csrf
+
+                <input type="text" name="name" id="" placeholder="Nombre de la etiqueta" class="rounded-l bg-gray-200 p-4 w-full outline-none">
+                <input type="submit" value="Enviar" class="rounded-r px-8 bg-blue-500 text-white outline-none cursor-pointer">
+
+            </form>
+
+            <h4 class="text-lg text-center mb-4">Listado de etiquetas</h4>
+            <table>
+                @forelse ($tags as $tag )
+
+                <tr>
+                    <td class="px-4 py-2">
+                        {{$tag->name}}
+                    </td>
+                    <td class="px-4 py-2">
+                        {{$tag->slug}}
+                    </td>
+                    <td class="px-4 py-2">
+                        <form action="tags/{{$tag->id}}" method="POST">
+                            
+                            @csrf
+                            @method('DELETE')
+
+                            <input type="submit" value="Eliminar" class="px-4 rounded bg-red-500 text-white cursor-pointer">
+                        
+                        </form>
+                    </td>
+                </tr>
+                    
+                @empty
+                    <tr>
+                        <td>
+                            <p>
+                                No hay etiquetas
+                            </p>
+                        </td>
+                    </tr>
+                @endforelse
+            </table>
+        </div>
+    </body>
+</html>
